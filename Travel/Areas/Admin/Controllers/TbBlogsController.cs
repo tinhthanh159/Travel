9eeb355ef2366b39bd0c9f80642fc08ecb1cad1a
Bùi Tĩnh <buitinh@gmail.com>
2025-01-13 08:48:47 +0700

    ok

---
+++ Travel/Areas/Admin/Controllers/TbBlogsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Travel.Models;
+
+namespace Travel.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TbBlogsController : Controller
+    {
+        private readonly TravelTourContext _context;
+
+        public TbBlogsController(TravelTourContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/TbBlogs
+        public async Task<IActionResult> Index()
+        {
+            var travelTourContext = _context.TbBlogs.Include(t => t.Account);
+            return View(await travelTourContext.ToListAsync());
+        }
+
+        // GET: Admin/TbBlogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbBlog = await _context.TbBlogs
+                .Include(t => t.Account)
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+            if (tbBlog == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbBlog);
+        }
+
+        // GET: Admin/TbBlogs/Create
+        public IActionResult Create()
+        {
+            ViewData["AccountId"] = new SelectList(_context.TbAccounts, "AccountId", "Title");
+            return View();
+        }
+
+        // POST: Admin/TbBlogs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BlogId,Title,Alias,CategoryId,Description,Detail,Image,Topic,SeoTitle,SeoDescription,SeoKeywords,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,AccountId,IsActive")] TbBlog tbBlog)
+        {
+            if (ModelState.IsValid)
+            {
+                tbBlog.Alias = Travel.Utilities.Function.TitleSlugGenerationAlias(tbBlog.Title);
+                _context.Add(tbBlog);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.TbAccounts, "AccountId", "AccountId", tbBlog.AccountId);
+            return View(tbBlog);
+        }
+
+        // GET: Admin/TbBlogs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbBlog = await _context.TbBlogs.FindAsync(id);
+            if (tbBlog == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.TbAccounts, "AccountId", "Title", tbBlog.AccountId);
+            return View(tbBlog);
+        }
+
+        // POST: Admin/TbBlogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BlogId,Title,Alias,CategoryId,Description,Detail,Image,Topic,SeoTitle,SeoDescription,SeoKeywords,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,AccountId,IsActive")] TbBlog tbBlog)
+        {
+            if (id != tbBlog.BlogId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tbBlog);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TbBlogExists(tbBlog.BlogId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.TbAccounts, "AccountId", "AccountId", tbBlog.AccountId);
+            return View(tbBlog);
+        }
+
+        // GET: Admin/TbBlogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbBlog = await _context.TbBlogs
+                .Include(t => t.Account)
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+            if (tbBlog == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbBlog);
+        }
+
+        // POST: Admin/TbBlogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tbBlog = await _context.TbBlogs.FindAsync(id);
+            if (tbBlog != null)
+            {
+                _context.TbBlogs.Remove(tbBlog);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TbBlogExists(int id)
+        {
+            return _context.TbBlogs.Any(e => e.BlogId == id);
+        }
+    }
+}
---
+++ Travel/Controllers/DestinationControler.cs
@@ -0,0 +1,38 @@
+﻿using Travel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Travel.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly TravelTourContext _context;
+        public DestinationController(TravelTourContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/destination/-{id}.html")]
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbDestinations == null)
+            {
+                return NotFound();
+            }
+            var blog = await _context.TbDestinations
+                .FirstOrDefaultAsync(m => m.DestinationId == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            
+            return View(blog);
+        }
+
+
+    }
+}
